a492c4b67b307c446fd31e45924ce72f01d3129c
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-04-19 21:21:52 +0700

    chore [AR]: manage branch and edit theme data

---
+++ lib/common/color_value.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorValue {
+
+  static const Color primaryColor = Color(0xFF009245);
+  static const Color secondaryColor = Color(0xFF0E4F55);
+  static const Color tertiaryColor = Color(0xFFEDB46A);
+  static const Color quaternaryColor = Color(0xFFEE6C4D);
+  static const Color neutralColor = Color(0xFF35363B);
+
+}
