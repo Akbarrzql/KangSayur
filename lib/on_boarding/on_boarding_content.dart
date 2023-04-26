0efde70e65120466961caa8192d8d5bc938a025c
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-04-26 08:16:29 +0700

    fix ui on boarding

---
+++ lib/on_boarding/on_boarding_content.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class OnboardingContents {
+  final String title;
+  final String image;
+  final String desc;
+
+  OnboardingContents(
+      {required this.title, required this.image, required this.desc});
+}
+
+List<OnboardingContents> contents = [
+  OnboardingContents(
+    title: "Kualitas Terbaik Dari Petani",
+    image: "assets/images/boarding_one.png",
+    desc: "Kami menghadirkan produk-produk segar dan berkualitas tinggi dari petani & peternak terbaik di Indonesia.",
+  ),
+  OnboardingContents(
+    title: "Kirim Sesuai Jadwal",
+    image: "assets/images/boarding_two.png",
+    desc: "Dengan fitur langganan kamu dapat mudah untuk memenentukan jadwal pengiriman sesui hari yang kamu tentukan.",
+  ),
+  OnboardingContents(
+    title: "Petani Sejahtara & Senang",
+    image: "assets/images/boarding_three.png",
+    desc: "Setiap penjualan para petani akan sangat terbantu.",
+  ),
+];
