348eaa042688039c0c92cd7d8b55719f0af8758e
Nabil <nabilrajankanz@gmail.comm>
2023-05-09 08:24:17 +0700

    [NR] add on tap on promo_list

--- lib/UI/bottom_nav/items/home/promo_list.dart
+++ lib/UI/bottom_nav/items/home/promo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kangsayur/UI/detail/detail.dart';
 import 'package:kangsayur/common/color_value.dart';
 
 class Promo_List extends StatefulWidget {
@@ -17,137 +18,145 @@ class _Promo_ListState extends State<Promo_List> {
       shrinkWrap: true,
       itemCount: 4,
       itemBuilder: (context, index) {
-        return Stack(
-          children: [
-            Container(
-                margin: EdgeInsets.fromLTRB(24, 0, 0, 10),
-                width: 145,
-                height: 248,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.25),
-                      spreadRadius: 0,
-                      blurRadius: 1,
-                      offset: Offset(0, 1), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          "assets/images/wortel.png",
-                          width: 145,
-                          height: 112,
-                          fit: BoxFit.cover,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(9, 0, 9, 0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    height: 9,
-                                  ),
-                                  Text(
-                                    "20 km",
-                                    style: TextStyle(
-                                      fontSize: 8,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  Text(
-                                    "Wortel lokal 1/kg",
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                  Text(
-                                    "Kebun Endah",
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(9, 0, 9, 7),
-                      child: Column(
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Detail()),
+            );
+          },
+          child: Stack(
+            children: [
+              Container(
+                  margin: EdgeInsets.fromLTRB(24, 0, 0, 10),
+                  width: 145,
+                  height: 248,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        spreadRadius: 0,
+                        blurRadius: 1,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            "Rp. 12.000",
-                            style: TextStyle(
-                                fontSize: 8,
-                                color: Colors.black,
-                                fontWeight: FontWeight.normal,
-                                decoration: TextDecoration.lineThrough),
+                          Image.asset(
+                            "assets/images/wortel.png",
+                            width: 145,
+                            height: 112,
+                            fit: BoxFit.cover,
                           ),
-                          Text(
-                            "Rp. 12.000",
-                            style: TextStyle(
-                                fontSize: 12,
-                                color: Color(0xff3D5A80),
-                                fontWeight: FontWeight.bold),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(9, 0, 9, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: 9,
+                                    ),
+                                    Text(
+                                      "20 km",
+                                      style: TextStyle(
+                                        fontSize: 8,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Wortel lokal 1/kg",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      "Kebun Endah",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                )),
-            Positioned(
-              top: 0,
-              right: 0,
-              child: Container(
-                width: 70,
-                height: 23,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15),
-                      topRight: Radius.circular(5)),
-                  color: Color(0xff3D5A80),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "20% ",
-                      style: TextStyle(
-                          fontSize: 12,
-                          color: ColorValue.quaternaryColor,
-                          fontWeight: FontWeight.bold),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(9, 0, 9, 7),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Rp. 12.000",
+                              style: TextStyle(
+                                  fontSize: 8,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal,
+                                  decoration: TextDecoration.lineThrough),
+                            ),
+                            Text(
+                              "Rp. 12.000",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Color(0xff3D5A80),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  width: 70,
+                  height: 23,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        topRight: Radius.circular(5)),
+                    color: Color(0xff3D5A80),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "20% ",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: ColorValue.quaternaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
 
-                    Text(
-                      "OFF",
-                      style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                      Text(
+                        "OFF",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
